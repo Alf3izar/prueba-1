3afc1eae6a3d3cb4ded78e61a70d1b229b773090
Alejandro-VP <alejandrovp1733@gmail.com>
2022-03-09 12:50:04 +0100

    V1.1

--- PruebaGit/src/pruebasGit/PruebaHelloWorld.java
+++ PruebaGit/src/pruebasGit/PruebaHelloWorld.java
@@ -5,6 +5,9 @@ public class PruebaHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola mundo");
+System.out.println("Soy Alejandro");
+System.out.println("Nos vemos pronto");
+
 	}
 
 }
